f76c8708bd2d1863cb7fadac74e69f4728f73d6f
gobirg3108 <gobigobi3108@gmail.com>
2024-10-24 21:40:29 +0530

    new Project Dice

--- JavaScript/26.Rest Parameter/script.js
+++ JavaScript/26.Rest Parameter/script.js
@@ -2,4 +2,56 @@
 //                   number of arguments by bundling them into an array.
 
 //                   spread = expands an array into separate elements
-//                   rest = bundles separate elements into an array 
+//                   rest = bundles separate elements into an array
+
+// Ex : 1
+
+// function openFridge(...foods) {
+//   console.log(...foods);
+// }
+// function getFoods(...foods) {
+//   return foods;
+// }
+// const food1 = "pizza";
+// const food2 = "hamburger";
+// const food3 = "hotdog";
+// const food4 = "sushi";
+
+// const foods = getFoods(food1, food2, food3, food4);
+// console.log(foods);
+
+// openFridge(food1, food2, food3, food4);
+
+// Ex : 2
+
+// function sum(...numbers) {
+//   let result = 0;
+//   for (let number of numbers) {
+//     result += number;
+//   }
+//   return result;
+// }
+
+// const total = sum(1,2,3,4);
+
+// console.log(`Your Total is $${total}`);
+
+// function getAverage(...numbers) {
+//   let result = 0;
+//   for (let number of numbers) {
+//     result += number;
+//   }
+//   return result / numbers.length;
+// }
+
+// const total = getAverage(80,54,23,58);
+
+// console.log(`Your Total is $${total}`);
+
+// Ex:3
+function combineStrings(...strings) {
+  return strings.join(" ");
+}
+const fullName = combineStrings("Mr.", "Gobinath", "Ramalingam");
+
+console.log(fullName);
